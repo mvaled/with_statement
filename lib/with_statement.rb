a465395874ade2cbd420519870da94520703adae
Manuel Vázquez Acosta <mva.led@gmail.com>
2010-12-28 15:15:55 -0500

    Adding support for several context managers.
    With this change a sentence like:
    
       with a, b do |x, y|
         # do something with x and y
       end
    
    it's semantically equivalent to:
    
       with a do |x|
         with b do |y|
           # do something to x and y
         end
       end
    
    Notice that if a.acquire succeeds and b.acquire fails, a should be
    released, but b should not.
    
    The general syntax now is:
    
       with(*managers){|*resources|
       }

--- lib/with_statement.rb
+++ lib/with_statement.rb
@@ -3,7 +3,23 @@ module Kernel
   # Similar to Python's with statement. This statement enters the
   # context of an object +resource+, executes the block, and exits the
   # context.
-  def with(resource)
+  def with(*resources, &block)
+    raise SyntaxError, 'with statement called with no arguments' if resources.size == 0
+    if resources.size == 1
+      simple_with(resources.shift, &block)
+    else
+      simple_with(resources.shift) do |what|
+        with(*resources) {|*rs|
+          args = [what] + rs
+          block.call(*args)
+        }
+      end
+    end
+  end
+
+  private
+
+  def simple_with(resource, &block)
     what = resource.acquire
     begin
       yield(what)
@@ -12,4 +28,3 @@ module Kernel
     end
   end
 end
-
--- test/test_with_statement.rb
+++ test/test_with_statement.rb
@@ -2,6 +2,8 @@ require 'helper'
 
 class TestWithStatement < Test::Unit::TestCase
   context "A simple non-reentrant resource class" do
+    class ResourceException < Exception; end
+
     class Resource
       attr_reader :locked
 
@@ -10,13 +12,13 @@ class TestWithStatement < Test::Unit::TestCase
       end
 
       def acquire
-        raise Exception.new "Already acquired" if @locked
+        raise ResourceException.new "Already acquired" if @locked
         @locked = true
         self
       end
 
       def release
-        raise Exception.new "Not acquired" unless @locked
+        raise ResourceException.new "Not acquired" unless @locked
         @locked = false
       end
 
@@ -25,20 +27,37 @@ class TestWithStatement < Test::Unit::TestCase
       end
     end
 
+    class LockedResource < Resource
+      def initialize
+        super
+        @locked = true
+      end
+    end
+
+    class WeirdException < Exception; end
+
     setup do
       @resource = Resource.new
+      @inner = Resource.new
+      @locked = LockedResource.new
     end
 
     should "be locked within with statement, and release outside it" do
-      with @resource do 
+      with @resource do
         assert @resource.locked
       end
       assert !@resource.locked
     end
 
+    should 'raise an exception on a locked resource' do
+      assert_raise ResourceException do
+        with @locked do; end
+      end
+    end
+
     should "raise an exception when double locked" do
       with @resource do
-        assert_raise Exception do 
+        assert_raise ResourceException do
           with @resource do; end
         end
       end
@@ -48,6 +67,50 @@ class TestWithStatement < Test::Unit::TestCase
       with @resource do |what|
         assert what == @resource
         assert what.do_something
+      end
+    end
+
+    should "allow nested withs" do
+      with @resource do |a|
+        with @inner do |b|
+          assert @resource.locked
+          assert @inner.locked
+        end
+        assert @resource.locked
+        assert !@inner.locked
+      end
+    end
+
+    should "allow multiple context managers" do
+      with @resource, @inner do |a, b, *others|
+        assert a == @resource
+        assert b == @inner
+        assert others == [], 'no more resources'
+        assert @resource.locked
+        assert @inner.locked
+      end
+      assert !@resource.locked
+      assert !@inner.locked
+    end
+
+    should 'release the first resource if the second fails to enter' do
+      assert_raise ResourceException do
+        with @resource, @locked do
+          raise StandardError, 'This exception should not be raised; because this code should never be called'
+        end
+      end
+      assert !@resource.locked
+    end
+
+    should 'raise a SyntaxError when no context managers are given' do
+      assert_raise SyntaxError do
+        with {}
+      end
+    end
+
+    should 'raise an exception if no block is given' do
+      assert_raise LocalJumpError do
+        with @resource
       end
     end
   end
